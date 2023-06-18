88baefd9f27e64d974a6ee9ef512dc924e91486c
Rolands <rolands.alksnis@gmail.com>
2023-06-18 23:37:21 +0300

    Added routes, views and model

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,18 @@
+<h1> {{ $heading }} </h1>
+
+@unless(count($listings) == 0)
+
+@if(count($listings) == 0)
+    <p>No listings </p>
+@endif
+
+@foreach($listings as $listing)
+<h2>
+    <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
+</h2>
+    <p>{{$listing['description']}}</p>
+@endforeach
+
+@else
+<p>No listings found</p>
+@endunless
