d3dad7596bcdb74413d10fd16daf648ca85e199a
Rolands <rolands.alksnis@gmail.com>
2023-06-19 22:50:12 +0300

    Added tag and search filtering, improved design code placement in project

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+
+    public function scopeFilter($query, array $filters) {
+        // filter for tags
+        if($filters['tag'] ?? false) {
+            $query->where('tags', 'like', '%' . request('tag') . '%');
+        }
+
+        // filter for search bar
+        if($filters['search'] ?? false) {
+            $query->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('description', 'like', '%' . request('search') . '%')
+                ->orWhere('tags', 'like', '%' . request('search') . '%')
+                ->orWhere('location', 'like', '%' . request('search') . '%');
+        }
+    }
 }
