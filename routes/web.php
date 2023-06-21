b6b3461367196a263d098a153a5e3bc3dd48a459
Rolands <rolands.alksnis@gmail.com>
2023-06-22 01:17:37 +0300

    Added form update and deletion

--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,14 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 // Store listing data
 Route::post('/listings', [ListingController::class, 'store']);
 
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// Update listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// Delete listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
 // Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
